945a4db1594c375a4ec630edd0f23c349761de7d
creativemindod <creativemindod@gmail.com>
2023-04-18 17:55:25 +0530

    Updated api code

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Kernel;
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method == "POST") {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Methods: *");
+    header("Allow: *");
+}
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
